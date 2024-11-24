947771d0415f00fc8fb06ff85a20993dd0913d0a
Oleg Shilo <oleg.shilo@gmail.com>
2024-11-24 21:35:07 +1100

    - Last release choco spec update

--- choco/tools/chocolateyInstall.ps1
+++ choco/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 $packageName = 'CS-Syntaxer'
-$url = 'https://github.com/oleg-shilo/syntaxer.core/releases/download/v3.1.3.0/syntaxer.7z'
+$url = 'https://github.com/oleg-shilo/syntaxer.core/releases/download/v3.1.4.0/syntaxer.7z'
 
 Stop-Process -Name "syntaxer" -ErrorAction SilentlyContinue
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyEnvironmentVariable 'CSSYNTAXER_ROOT' $installDir User
 
-$checksum = '41E0B4E54E7E2312E85172C3A71A56FEB0634A5A80B4461C5A008AFE4C432E78'
+$checksum = '5118D27B449C943A33A491922B2750D9552C529D31EA55C3D3792B1518FA6B1B'
 $checksumType = "sha256"
 
 # Download and unpack a zip file
